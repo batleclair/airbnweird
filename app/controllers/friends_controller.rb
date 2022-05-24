6cf733ac2d48cdeed864af89e09e7f72d77f4129
Mouna Tricha <mouna.tricha@live.fr>
2022-05-24 11:39:14 +0200

    friend controller and views

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,7 @@
 class FriendsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show search]
+  before_action :set_user, only: [:show]
+
   def index
     @friends = Friend.all
   end
@@ -10,10 +13,35 @@ class FriendsController < ApplicationController
   def create
     @friend = Friend.new(friend_params)
     @friend.save
-    redirect_to friend_path(@friend)
+    redirect_to new_user_friend_path(@friend)
+  end
+
+  def show
+    @friend = Friend.find(params[:id])
+  end
+
+  def edit
+    @friend = Friend.find(params[:id])
+    authorize @friend
+  end
+
+  def update
+    @friend = Friend.find(params[:id])
+    @friend.update(friend_params)
+    redirect_to new_user_friend_path(@friend)
+  end
+
+  def destroy
+    @friend = Friend.find(params[:id])
+    @friend.destroy
+    redirect_to friends_path, status: :see_other
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
   def friend_params
     params.require(:friend).permit(:name, :description, :category, :language, :price)
