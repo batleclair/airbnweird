119b32e782b7ede1f8c54b60aca9f25d45d19db0
Baptiste Clair <baptisteclair@gmail.com>
2022-05-24 12:00:39 +0200

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require "open-uri"
+
+puts 'seeding weird stuff'
+
+User.delete_all
+Friend.delete_all
+
+5.times do
+  user = User.new
+  user.email = Faker::Internet.email
+  user.password = 'password123'
+  user.name = Faker::Name.name
+  user.save
+end
+
+10.times do
+  friend = Friend.new
+  friend.name = Faker::Games::Pokemon.name
+  friend.category = Faker::Creature::Animal.name
+  friend.user = User.all.sample
+  friend.price = rand(50..250)
+  friend.description = Faker::TvShows::RickAndMorty.quote
+  file_path = Faker::LoremFlickr.image(size: "200x200", search_terms: ['animal'])
+  file = URI.open(file_path)
+  friend.photo.attach(io: file, filename: "#{friend.name}-#{friend.user.name}", content_type: "image/jpg")
+  friend.save
+end
+
+puts 'all done? see last created friend'
+puts Friend.last.name
