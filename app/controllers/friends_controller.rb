d17a3e54a124c20f5ecd8a7bdd5e14700b6b2aa0
Keyvan Sabras <keyvan.sabras@edu.escp.eu>
2022-05-24 12:16:34 +0200

    add-pundit & gem

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,35 +3,41 @@ class FriendsController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
+    authorize @friend
     @friends = Friend.all
   end
 
   def new
+    authorize @friend
     @friend = Friend.new
   end
 
   def create
+    authorize @friend
     @friend = Friend.new(friend_params)
     @friend.save
     redirect_to new_user_friend_path(@friend)
   end
 
   def show
+    authorize @friend
     @friend = Friend.find(params[:id])
   end
 
   def edit
-    @friend = Friend.find(params[:id])
     authorize @friend
+    @friend = Friend.find(params[:id])
   end
 
   def update
+    authorize @friend
     @friend = Friend.find(params[:id])
     @friend.update(friend_params)
     redirect_to new_user_friend_path(@friend)
   end
 
   def destroy
+    authorize @friend
     @friend = Friend.find(params[:id])
     @friend.destroy
     redirect_to friends_path, status: :see_other
