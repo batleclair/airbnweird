6a30e4c008465243154edaf226f776fab54cdef0
Baptiste Clair <baptisteclair@gmail.com>
2022-05-25 13:01:48 +0200

    ignore changes do not merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
   def home
     @friends = Friend.all.sample(8)
   end
+
+  def dashboard
+    @bookings = Booking.where(:user_id == current_user)
+    @own_friends = Friend.where(:user == current_user)
+    raise
+    # @sales = Booking.where(@own_friends.find(:friend_id))
+  end
 end
